109f27b27f173fd2c173671c57201f5107b75e46
Daniel Lopez <danlop14@ucm.es>
2021-03-13 17:10:18 +0100

    Avanzo el index y creo SecurityConfig para evitar el login (provisionalmente)

---
+++ src/main/java/es/ucm/fdi/iw/gotour/SecurityConfig.java
@@ -0,0 +1,18 @@
+package es.ucm.fdi.iw.gotour;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+            .antMatchers("/**").permitAll()// "/**" = todo
+            .anyRequest().authenticated()
+            .and()
+        .formLogin()
+            .permitAll();
+    }
+}
