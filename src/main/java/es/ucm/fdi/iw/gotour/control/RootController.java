735a6480b0a5405ada6dad59c3cb94c1e7350635
lucialat <61499926+lucialat@users.noreply.github.com>
2021-03-14 18:49:13 +0100

    vista perfil

--- src/main/java/es/ucm/fdi/iw/gotour/control/RootController.java
+++ src/main/java/es/ucm/fdi/iw/gotour/control/RootController.java
@@ -1,5 +1,7 @@
 package es.ucm.fdi.iw.gotour.control;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,17 +10,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class RootController {
     
-    public static class Persona {
+    /*public static class Usuario {
         private String nombre;
-        private String apellido;
-        public Persona(String nombre, String apellido ) {
+        private String apellidos;
+        private String rol;
+        private Date membresia;
+        private List<Tour> ToursDisp;
+        private List<Tour> ToursCerrados;
+
+
+
+        public Usuario(String nombre, String apellidos, String rol) {
             this.nombre = nombre;
-            this.apellido = apellido;
+            this.apellidos = apellidos;
+            this.rol = rol;
         }
 
         public String getNombre() { return nombre; }
-        public String getApellido() { return apellido; }
-    }
+        public String getApellido() { return apellidos; }
+        public String getRol() { return rol; }
+    }*/
     
 
     @GetMapping("/")            // <-- en qué URL se expone, y por qué métodos (GET)        
@@ -43,6 +54,24 @@ public class RootController {
     @GetMapping("/perfil")
     public String perfil(Model model)
     {  
+        Date membresia = new Date();
+        int id_usuario = 1;
+        int id_sesion = 1;
+        String[] tourD1 = {"Visita guiada del Coliseo", "Roma", "4 huecos disponibles"};
+        String[] tourD2 = {"Madrid del siglo XIX", "Madrid", "9 huecos disponibles"};
+        String[] tourC1 = {"Paseo cultural por Santillana del Mar", "Santillana del Mar"};
+        String[] resenya1 = {"3 estrellas", "pedro", "Visita guiada del Coliseo", "Muy buen tour. El guía es muy agradable."};
+        model.addAttribute("nombre", "Juan");
+        model.addAttribute("apellidos", "Shánchez Pisuerga");
+        if (id_usuario == id_sesion) model.addAttribute("propio", true);
+        else model.addAttribute("propio", false);
+        model.addAttribute("membresia", membresia);
+        model.addAttribute("rol", "guia");
+        model.addAttribute("tourD1",tourD1);
+        model.addAttribute("tourD2",tourD2);
+        model.addAttribute("tourC1",tourC1);
+        model.addAttribute("resenya1",resenya1);
+        model.addAttribute("estrellas", 3);
         return "perfil";
     }
 
