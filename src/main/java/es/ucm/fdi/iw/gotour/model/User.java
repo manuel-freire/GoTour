f3a66115b2f3958467220b46bfea9b92ad492296
Usuario generico alumno <alumno@ubuntu2004.localdomain>
2021-03-19 18:23:02 +0100

    Add: clase Tour

--- src/main/java/es/ucm/fdi/iw/gotour/model/User.java
+++ src/main/java/es/ucm/fdi/iw/gotour/model/User.java
@@ -1,17 +1,9 @@
 package es.ucm.fdi.iw.gotour.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.ManyToMany;
+import javax.persistence.*;
 import java.util.List;
 import java.util.ArrayList;
-
 import lombok.Data;
-
 
 @Entity
 @Data
@@ -20,23 +12,22 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-/*
+
 	
 	private String nombre;
 	private String apellidos;
-	
+
 	
 	@OneToMany
+	@JoinColumn(name="guia_id")
 	private List<Tour> toursofrecidos=new ArrayList<>();
 
     @ManyToMany
 	private List<Tour> tourscontratados=new ArrayList<>();
 
-	@ManyToMany
-	private List<Review> reviews=new ArrayList<>();
-
 	@Override
 	public String toString() {
 		return "";
-	}	*/	
+	}	
 }
+
--- target/classes/import.sql
+++ target/classes/import.sql
@@ -1,2 +1,7 @@
 insert into car values (1, 'Chevy', 'Impala');
 insert into car values (2, 'Tesla', 'Roadster');
+insert into user values(1, 'ROman', 'Alex');
+insert into tour values(1, 'Madrid', 'Descripcion', '1', '2021-04-17 16:00:00', '2021-04-17 00:00:00', 'Plaz de torros', '/img', 'España', 'img', 15.50,  'Tour1', 1 );
+insert into tour_tags values (1, 'Madrid');
+insert into tour_tags values (1, 'Edificio');
+insert into tour_tags values (1, 'Familiar');
